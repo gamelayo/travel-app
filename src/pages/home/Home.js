f4e0c7a22a066568fa9ec2041e56fed0520cbc7c
Gamel Ayodele <ayodelegamel@gmail.com>
2023-04-24 10:17:05 +0100

    new commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,10 +4,14 @@ import Destination from "../../component/destination/Destination";
 import Search from "../../component/search/Search";
 import Selects from "../../component/select/Selects";
 import ImgCarousel from "../../component/carousel/ImgCarousel";
-
+import ScrollToTop from "react-scroll-up";
+import { BsArrowUpCircle } from "react-icons/bs";
 const Home = () => {
   return (
     <>
+      <ScrollToTop showUnder={160}>
+        <BsArrowUpCircle size={25} />
+      </ScrollToTop>
       <Hero />
       <Destination />
       <Search />
